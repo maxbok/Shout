04f8f40d6233978494a20d32042bf04f4c72e645
Maxime Bokobza <max@codehawks.eu>
2022-02-08 22:39:07 +0100

    Use prebuild libssh2 from DimaRU

--- Package.swift
+++ Package.swift
@@ -5,17 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "Shout",
-    platforms: [
-        .macOS(.v10_10)
-    ],
     products: [
         .library(name: "Shout", targets: ["Shout"]),
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "1.0.46"),
+        .package(url: "https://github.com/DimaRU/Libssh2Prebuild.git", .branch("master"))
     ],
     targets: [
-        .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
         .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
         .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
     ]
